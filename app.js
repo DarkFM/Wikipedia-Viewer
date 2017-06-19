90885907b2fd61769212523104714d158c349711
DarkFM <c.bestmann@hotmail.com>
2017-06-18 18:01:13 -0700

    fading out results & no new search on same search query

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var searchBox = document.querySelector(".query");
 var button = document.querySelector(".search-btn");
 var contents = document.querySelector(".contents");
 var form = document.getElementById('myform');
-
+var header = document.querySelector("header");
+var searchedItem = "";
 
 searchBox.addEventListener('focus', function(event) {
     searchBox.style.borderColor = "goldenrod";
     button.classList.add("move-inside");
-    // console.log(searchBox);
+
 });
 
 searchBox.addEventListener('blur', function(event) {
@@ -20,18 +21,46 @@ searchBox.addEventListener('blur', function(event) {
 });
 
 form.addEventListener("submit", function(event) {
-    event.preventDefault();
+    event.preventDefault(); // prevents the deafult submit action on form which reloads pages
+    console.log(searchedItem);
+    if(searchBox.value == searchedItem){
+        return;
+    }
+
+    if (searchBox.value == "") {
+        fadeOut();
+        return;
+    }
     contents.innerHTML = "";
     getEntries(searchBox.value);
 });
 
-button.addEventListener("click", function (event) {
+// used to fade out results when no input is given
+function fadeOut() {
+    var wrap = document.querySelector(".content-wrapper");
+    wrap.classList.add("fadeOutDown");
+    setTimeout(function() {
+        wrap.classList.add("remove-content");
+    }, 500);
+}
+
+button.addEventListener("click", function(event) {
+    if(searchBox.value == searchedItem){
+        return;
+    }
+
+    if (searchBox.value == "") {
+        fadeOut();
+        return;
+    }
+
     contents.innerHTML = "";
     getEntries(searchBox.value);
 });
 
-function getEntries(Searchquery) {
+function getEntries(searchQuery) {
 
+    searchedItem = searchQuery;
     $.ajax({
         type: "GET",
         url: "https://en.wikipedia.org/w/api.php",
@@ -46,19 +75,23 @@ function getEntries(Searchquery) {
         data: {
             "action": "opensearch",
             "format": "json",
-            "search": Searchquery,
+            "search": searchQuery,
             "namespace": "0",
             limit: "15",
             profile: "normal"
         },
         success: function(data) {
-            data = data.filter(function (val, i, arr) {
+            data = data.filter(function(val, i, arr) {
                 return (typeof val === "object");
             });
 
+            var topDiv = document.createElement("div");
+            topDiv.classList.add("content-wrapper");
+            topDiv.classList.add("animated");
+
             for (var i = 0; i < data[0].length; i++) {
                 var div = document.createElement('div');
-                div.classList.add("sec-container")
+                div.classList.add("article-container")
 
                 var article = document.createElement('article');
                 var h2 = document.createElement("h2");
@@ -69,22 +102,21 @@ function getEntries(Searchquery) {
                 para.classList.add("description");
                 h2.innerText = data[0][i];
 
-                para.innerText = ( !data[1][i] ) ? "No description to Display" : data[1][i];
+                para.innerText = (!data[1][i]) ? "No description to display" : data[1][i];
                 link.href = data[2][i];
-                link.setAttribute("target","_blank");
+                link.setAttribute("target", "_blank");
 
                 link.appendChild(h2);
                 link.appendChild(para);
 
                 article.appendChild(link);
                 article.classList.add("animated");
-                article.classList.add( (i%2) ? "slideInLeft" : "slideInRight");
+                article.classList.add((i % 2) ? "slideInLeft" : "slideInRight");
                 div.appendChild(article);
 
-
-                contents.appendChild(div);
-                // console.log(article);
+                topDiv.appendChild(div);
             }
+            contents.appendChild(topDiv)
         }
     });
 
